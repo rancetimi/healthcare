6fb7ab5270711a2ed99409c2d80e192dbafddf74
Kornerstone Counselling Centre <rancecreation@gmail.com>
2022-01-10 01:48:45 +0100

    Entry point for React App

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyApp from './components/MyApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MyApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
