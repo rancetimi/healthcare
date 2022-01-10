98a4a58e978eca22a2392876993861e40e73ab6d
Kornerstone Counselling Centre <rancecreation@gmail.com>
2022-01-10 01:49:02 +0100

    MyApp component specially written by me and Emma

---
+++ src/components/MyApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../layout/Layout";
+import Featured from "./Featured"
+
+const MyApp = () => {
+    return (
+        <Layout>
+            <Featured />
+        </Layout>
+    );
+}
+
+export default MyApp;
